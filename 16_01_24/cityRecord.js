88429012d37e2e0a873b6dc7fbab63a96cad0a29
KrasenaKirilova <krasena.kirilova@gmail.com>
2024-01-17 19:30:42 +0200

    Create cityRecord.js

---
+++ 16_01_24/cityRecord.js
@@ -0,0 +1,21 @@
+function createCity (name, population, treasury) {
+   
+    const city = {
+        name,
+        population, treasury
+    }
+    
+return city;
+}
+
+
+
+createCity('Tortuga',
+7000,
+15000
+);
+
+createCity('Santo Domingo',
+12000,
+23500
+);
